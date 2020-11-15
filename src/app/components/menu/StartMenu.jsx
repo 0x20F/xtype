9b4fbd8db10efcb4c5386d423e3c40474c18c7d5
Poly <alexhexan22@gmail.com>
2020-11-15 14:11:51 +0100

    Major design changes

--- src/app/components/menu/StartMenu.jsx
+++ src/app/components/menu/StartMenu.jsx
@@ -22,8 +22,12 @@ class StartMenu extends AnimatedComponent {
                 </header>
                 
                 <Button hint='P' text='Singleplayer' onClick={ this.props.startHandler }/>
-                <Button hint='M' text='Multiplayer' onClick={ () => console.log('Multiplayer is WIP') }/>
+                <Button disabled={true} hint='M' text='Multiplayer' onClick={ () => console.log('Multiplayer is WIP')}/>
+                
+                <div className='spacer'></div>
+                
                 <Button mini={true} hint='S' text='Settings' onClick={ this.props.settingsHandler }/>
+                <Button disabled={true} mini={true} hint='L' text='Leaderboard' onClick={ () => console.log('Leaderboard is WIP') }/>
             </div>
         );
     }
