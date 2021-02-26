16cf9798cc38c7008a303bdc78e19ce472292418
0x20F <10909659+0x20F@users.noreply.github.com>
2021-02-26 10:19:46 +0000

    Deploying to pages from  @ d5d0e0b3254311af27575b3d109eb4f72c67b5a3 🚀

--- src/app/foundation/WordList.js
+++ src/app/foundation/WordList.js
@@ -6,7 +6,6 @@ class WordList {
     text;
     list;
     counter = 0;
-    orderedMode = false;
 
 
     load = async () => {
@@ -27,8 +26,53 @@ class WordList {
      */
     split = () => {
         // lmao
-        this.list = this.text.split(/[\s,.()\/\\=?<>`´!"#¤%&\[\]§_:;\*'^~¨\-\+{}|]/gm);
-        this.list = this.list.filter(w => w.length > 0);
+        let words = this.text
+            .split(/[\s,.()\/\\=?<>`´!"#¤%&\[\]§_:;\*'^~¨\-\+{}|]/gm)
+            .filter(w => w.length > 0)
+            .map(w => w.toLowerCase());
+
+        // An array for each character in the english alphabet.
+        // Horrible to look at but it works for now.
+        let list = [
+            [],[],[],[],[],[],[],
+            [],[],[],[],[],[],[],
+            [],[],[],[],[],[],[],
+            [],[],[],[],[]
+        ];
+
+        // Jesus
+        words.forEach(word => {
+            switch (true) {
+                case word.startsWith('a'): list[0].push(word); break;
+                case word.startsWith('b'): list[1].push(word); break;
+                case word.startsWith('c'): list[2].push(word); break;
+                case word.startsWith('d'): list[3].push(word); break;
+                case word.startsWith('e'): list[4].push(word); break;
+                case word.startsWith('f'): list[5].push(word); break;
+                case word.startsWith('g'): list[6].push(word); break;
+                case word.startsWith('h'): list[7].push(word); break;
+                case word.startsWith('i'): list[8].push(word); break;
+                case word.startsWith('j'): list[9].push(word); break;
+                case word.startsWith('k'): list[10].push(word); break;
+                case word.startsWith('l'): list[11].push(word); break;
+                case word.startsWith('m'): list[12].push(word); break;
+                case word.startsWith('n'): list[13].push(word); break;
+                case word.startsWith('o'): list[14].push(word); break;
+                case word.startsWith('p'): list[15].push(word); break;
+                case word.startsWith('q'): list[16].push(word); break;
+                case word.startsWith('r'): list[17].push(word); break;
+                case word.startsWith('s'): list[18].push(word); break;
+                case word.startsWith('t'): list[19].push(word); break;
+                case word.startsWith('u'): list[20].push(word); break;
+                case word.startsWith('v'): list[21].push(word); break;
+                case word.startsWith('w'): list[22].push(word); break;
+                case word.startsWith('x'): list[23].push(word); break;
+                case word.startsWith('y'): list[24].push(word); break;
+                case word.startsWith('z'): list[25].push(word); break;
+            }
+        });
+
+        this.list = list;
         console.log(this.list);
     }
 
@@ -53,16 +97,12 @@ class WordList {
             this.counter = 0;
         }
 
-        if (this.orderedMode) {
-            /**
-             * WIP:
-             * - This needs to return all words in a sentence
-             * - all ships in a wave should be the amount of words in a sentence
-             */
-            return this.list[this.counter++].trim();
-        }
+        let wordGroup = this.list[this.counter];
+        let word = _.sample(wordGroup).trim();
 
-        return _.sample(this.list).trim();
+        this.counter++;
+
+        return word;
     }
 }
 
