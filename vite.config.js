bf423fd6534437adea515b2f84bd8e90dc63262c
0x20F <10909659+0x20F@users.noreply.github.com>
2022-12-21 12:30:17 +0000

    Deploying to pages from  @ 752fbd2547ccbc924b2d4b205c1bd04cefd170a5 🚀

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 const path = require('path');
 
 export default defineConfig({
+	base: '/xtype/',
 	resolve: {
 		alias: {
 			'components' : path.resolve(__dirname, 'src/app/components'),
