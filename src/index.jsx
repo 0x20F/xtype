b75a45509bd7cb239502c5edc68e9c140277d390
0x20F <10909659+0x20F@users.noreply.github.com>
2022-12-21 12:16:56 +0000

    Deploying to pages from  @ 8dcc1b457d4d7400b0bed0b37082498099f96142 🚀

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './app/App'
-import 'styles/app'
+import 'styles/app.scss'
 
 
 ReactDOM.render(<App/>, document.getElementById('root'))
