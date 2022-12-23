c7afd115607b0650ce6085520d1f672f300a611d
0x20F <0x20fa@gmail.com>
2022-12-23 13:07:23 +0100

    Don't start in the leaderboard menu

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -28,7 +28,7 @@ export default class App extends Component {
             paused: false,
             started: false,
             inSettings: false,
-            inLeaderboard: true,
+            inLeaderboard: false,
             intermission: false,
             gameOver: false,
 
